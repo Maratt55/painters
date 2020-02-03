f070a3becc9e4cbd68cb6247a2d42fdbadf7e2f3
marat.bagdasaryan <marat.bagdasaryan@mail.ru>
2020-02-03 17:00:04 +0400

    Updated

--- src/main/java/com/test/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/test/config/CustomAuthenticationEntryPoint.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Returns a 401 error code (Unauthorized) to the client.
- */
+
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
